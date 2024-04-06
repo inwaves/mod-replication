e3f30c6cb27625bbbc4f992a57c269145ebaf5d6
Andrei Alexandru <inwaves@live.com>
2024-04-06 15:28:59 +0000

    Load GPT2 family models

--- train.py
+++ train.py
@@ -1,6 +1,23 @@
-from transformers import AutoConfig, AutoTokenizer, GPT2Model
+from transformers import AutoConfig, AutoTokenizer, GPT2LMHeadModel
 
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
-model_config = AutoConfig.from_pretrained("gpt2")
+model_aliases = {
+    "GPT2_SMALL": "gpt2", 
+    "GPT2_MEDIUM": "gpt2-medium", 
+    "GPT2_LARGE": "gpt2-large", 
+    "GPT2_XL": "gpt2-xl"
+}
 
-model = GPT2Model(model_config)
+tokenisers = []
+models = []
+for model, alias in model_aliases.items():
+    tokenizer = AutoTokenizer.from_pretrained(alias)
+    model_config = AutoConfig.from_pretrained(alias)
+
+    language_model = GPT2LMHeadModel(model_config)
+
+    tokenisers += [tokenizer]
+    models += [language_model]
+
+print(f"I initialised {len(tokenisers)} tokenisers and {len(models)} models...")
+print(tokenisers)
+print(models)
