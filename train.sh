cefd75d5971c0d34e53b2261d2a21d6a32097f14
danton267 <dan.anton267@gmail.com>
2024-04-08 17:57:12 +0000

    Refactor train.sh: Update command-line arguments for training script

--- train.sh
+++ train.sh
@@ -1,7 +1,9 @@
-python train.py
--- use_mod True
--- batch_size 32
--- capacity_fraction 0.125
--- epochs 10
--- model gpt2
--- log_every_n_steps 100
+#!/bin/bash
+
+python train.py \
+  --use_mod True \
+  --batch_size 32 \
+  --capacity_fraction 0.125 \
+  --epochs 10 \
+  --model gpt2 \
+  --log_every_n_steps 100
